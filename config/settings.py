6808f841663717ff36e00e165dd789a65dce3090
Ryan <ryandavidallen1@gmail.com>
2026-03-20 20:15:33 -0700

    Grabs full dataset and cleans up EDA.

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
 class Settings(BaseModel):
-    data_start_date: str = '2025-06-01'
-    date_end_date: str = '2025-06-02'
+    data_start_date: str = '2025-01-01'
+    date_end_date: str = '2025-12-31'
     data_csv_path: str = 'data/pitches.csv'
